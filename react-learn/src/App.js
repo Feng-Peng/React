2c78edde2c30a565dfbac2c04c7b3b288690544e
Feng-Peng <1559139360@qq.com>
2019-11-28 09:18:28 +0800

    6-7.实现withRouter

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from "./react-router-dom"
+import { BrowserRouter, Route, Switch, withRouter } from "./react-router-dom"
+
+function Comp(props) {
+    return <div>
+        {props.text}
+        <button onClick={() => {
+            props.history.push('/page2')
+        }}>到Page2</button>
+    </div>
+}
+
+const CompWithRouter = withRouter(Comp);
 
 function Page1() {
-    return <h1>Page1</h1>
+    return <h1>
+        Page1
+        <CompWithRouter text="abc" />
+    </h1>
 }
 
 function Page2() {
@@ -13,7 +27,6 @@ export default function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/page1" component={Page1} />
                 <Route path="/page1" component={Page1} />
                 <Route path="/page2" component={Page2} />
             </Switch>
--- react-learn/src/react-router-dom/index.js
+++ react-learn/src/react-router-dom/index.js
@@ -1,2 +1,3 @@
 export { default as BrowserRouter } from './BrowserRouter'
+export { withRouter } from '../react-router'
 export { Route, Switch } from '../react-router'
