94a2f1474be22e2414f816f4fcfe56f682be1b92
Feng-Peng <1559139360@qq.com>
2019-11-28 18:26:48 +0800

    7-1.Redux核心概念

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,27 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Route, Link, NavLink } from "./react-router-dom"
-
-function Page1() {
-    return <h1>Page1</h1>
-}
-
-function Page2() {
-    return <h1>Page2</h1>
-}
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Route path="/page1" component={Page1} />
-            <Route path="/page2" component={Page2} />
-            <div>
-                <NavLink to={{
-                    pathname: '/page1',
-                    search: '?a=1'
-                }}>到Page1</NavLink>
-                <br />
-                <NavLink to="/page2">到Page2</NavLink>
-            </div>
-        </BrowserRouter>
+        <div>
+            
+        </div>
     )
 }
