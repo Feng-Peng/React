a72f1c3328abd59dae7f093622d3d30d2ad0fd7a
Feng-Peng <1559139360@qq.com>
2019-12-01 14:32:33 +0800

    8-1.redux-logger

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -1,65 +1,17 @@
 import { createStore, bindActionCreators, applyMiddleware } from 'redux'
 import reducer from './reducer'
 import { createAddUserAction, createDeleteUserAction } from './action/userAction'
+import logger from 'redux-logger'
 
 
-const logger1 = store => next => action => {
-    console.log('中间件1');
-    console.log('旧数据', store.getState());
-    console.log('action', action)
-    next(action); // 此时调用的是中间件2
-    console.log('新数据', store.getState());
-    console.log("");
-}
+let store = createStore(reducer, applyMiddleware(logger));
 
-const logger2 = store => next => action => {
-    console.log('中间件2');
-    console.log('旧数据', store.getState());
-    console.log('action', action)
-    next(action);
-    console.log('新数据', store.getState());
-    console.log("");
-}
 
-// /**
-//  * 中间件函数
-//  * @param {*} store 
-//  */
-// function logger1(store) {
-//     return function (next) { // 此处的next就是dispatch函数
-//         return function (action) {
-//             console.log('中间件1');
-//             console.log('旧数据', store.getState());
-//             console.log('action', action)
-//             next(action);
-//             console.log('新数据', store.getState());
-//             console.log("");
-//         }
-//     }
-// }
-
-// function logger2(store) {
-//     return function (next) { // 此处的next就是dispatch函数
-//         return function (action) {
-//             console.log('中间件2');
-//             console.log('旧数据', store.getState());
-//             console.log('action', action)
-//             next(action);
-//             console.log('新数据', store.getState());
-//             console.log("");
-//         }
-//     }
-// }
-
-// 1.
-let store = createStore(reducer, applyMiddleware(logger1, logger2));
-
-// 2.
 // let store = applyMiddleware(logger1, logger2)(createStore)(reducer);
 
-// store.subscribe(() => {
-//     console.log(store.getState())
-// })
+store.subscribe(() => {
+    console.log(store.getState())
+})
 
 let userAction = {
     createAdd: createAddUserAction,
