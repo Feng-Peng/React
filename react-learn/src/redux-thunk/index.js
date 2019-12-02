53a36b58010a6030f9f7c6aa42ebc819115e2654
Feng-Peng <1559139360@qq.com>
2019-12-02 15:39:49 +0800

    增加了一些注释

--- react-learn/src/redux-thunk/index.js
+++ react-learn/src/redux-thunk/index.js
@@ -1,8 +1,10 @@
 function createThunkMiddleware(extra) {
     return store => next => action => {
+        // 如果action是一个函数就直接执行
         if (typeof action === 'function') {
             return action(store.dispatch, store.getState, extra);
         }
+        // action不是一个函数时就继续向后传递
         return next(action);
     }
 }
